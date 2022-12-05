981ec8099a33b9f0f31fd25eda03515a8fb1fa7e
sharmi91 <51904049+sharmi91@users.noreply.github.com>
2022-12-04 22:34:04 -0700

    final search file

--- ResearchMatchMaking/src/matchpackage/search/SearchAgent.java
+++ ResearchMatchMaking/src/matchpackage/search/SearchAgent.java
@@ -8,33 +8,20 @@ import matchpackage.database.Provider;
 import matchpackage.search.SortProvidersList;
 
 public class SearchAgent extends Agent {
-
 	protected void setup() {
-
 		System.out.println("I am a Search Agent");
 		System.out.println("This is my name : " + getAID().getName());
 		System.out.println("My global name is " + getAID().getLocalName());
 		System.out.println("My addresses are :" + getAID().getAllAddresses());
 		addBehaviour(new SortProviders());
-
 	}
-
 	private class SortProviders extends CyclicBehaviour {
-		
-
 		private int step = 0;
-
-		@Override
+		
 		public void action() {
-			// TODO Auto-generated method stub
-
 			ACLMessage msg = myAgent.blockingReceive();
 			System.out.println(msg);
-			
-			
 			ACLMessage reply = msg.createReply();
-			
-
 			reply.setContent(new SortProvidersList(msg.getContent()).sortProvidersList());
 			send(reply);
 
